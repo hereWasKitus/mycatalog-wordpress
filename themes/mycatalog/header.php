4096542592928e3d6a17156d7e96fe11257e25f3
hereWasKitus <superpants@ex.ua>
2020-09-22 16:19:02 +0300

    Build desktop header

--- themes/mycatalog/header.php
+++ themes/mycatalog/header.php
@@ -1,59 +1,53 @@
 <?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package mycatalog
- */
+  $user = is_user_logged_in() ? wp_get_current_user() : false;
+  $avatar_url = $user !== false ? get_avatar_url( $user -> ID ) : '';
+  $cart_count = '';
 
+  if ( $user !== false ) {
+    $wc_cart = new WC_Cart();
+    $cart_count = $wc_cart -> get_cart_contents_count();
+  }
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'mycatalog' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$mycatalog_description = get_bloginfo( 'description', 'display' );
-			if ( $mycatalog_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $mycatalog_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+  <header class="header <?= is_user_logged_in() ? 'is-logged-in' : '' ?>">
+    <div class="wrapper">
+      <div class="inner">
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'mycatalog' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+        <ul class="header__menu">
+          <li><a href="#" class="click-animation">pod</a></li>
+          <li><a href="#" class="click-animation">digital franchise</a></li>
+
+          <li class="header__logo">
+            <a href="<?= home_url() ?>"><img src="<?= get_template_directory_uri() . '/assets/images/logo.svg' ?>"></a>
+          </li>
+
+          <li><a href="#" class="click-animation">for advertisers</a></li>
+          <li><a href="#" class="click-animation">private label</a></li>
+        </ul>
+
+        <div class="header__user">
+          <a href="#" class="header__user-cart">
+            <img src="<?= get_template_directory_uri() . '/assets/images/basket.svg' ?>">
+            <span class="header__user-cart__counter"><?= $cart_count ?></span>
+          </a>
+          <a href="#" class="header__user-avatar">
+            <img src="<?= get_template_directory_uri() . '/assets/images/no-avatar.svg' ?>">
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </header><!-- #masthead -->
