566d40468c5cb90c04a18577e6372d1ab8b2cf75
hereWasKitus <superpants@ex.ua>
2020-09-23 10:01:20 +0300

    Build footer

--- themes/mycatalog/footer.php
+++ themes/mycatalog/footer.php
@@ -11,21 +11,64 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mycatalog' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'mycatalog' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'mycatalog' ), 'mycatalog', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+  <footer class="footer">
+    <div class="wrapper">
+      <div class="inner">
+
+        <div class="col footer__contacts">
+          <p class="col-heading">contacts</p>
+          <ul>
+            <li>City</li>
+            <li>Street</li>
+            <li>+7 974 87 982</li>
+            <li>irss2019@gmail.com</li>
+          </ul>
+        </div>
+        <div class="col footer__social">
+          <p class="col-heading">social</p>
+          <ul>
+            <li><a href="#"><img src="<?= get_template_directory_uri() . '/assets/images/social/facebook.svg' ?>"></a></li>
+            <li><a href="#"><img src="<?= get_template_directory_uri() . '/assets/images/social/linkedin.svg' ?>"></a></li>
+            <li><a href="#"><img src="<?= get_template_directory_uri() . '/assets/images/social/twitter.svg' ?>"></a></li>
+          </ul>
+        </div>
+        <div class="col footer__logo">
+          <img src="<?= get_template_directory_uri() . '/assets/images/logo-footer.svg' ?>">
+        </div>
+        <div class="col">
+          <p class="col-heading">news</p>
+          <ul>
+            <li>Num 1</li>
+            <li>Num 2</li>
+            <li>Num 3</li>
+            <li>Num 4</li>
+          </ul>
+        </div>
+        <div class="col">
+          <p class="col-heading">events</p>
+          <ul>
+            <li>Num 1</li>
+            <li>Num 2</li>
+            <li>Num 3</li>
+            <li>Num 4</li>
+          </ul>
+        </div>
+        <div class="col">
+          <p class="col-heading">blog</p>
+          <ul>
+            <li>Num 1</li>
+            <li>Num 2</li>
+            <li>Num 3</li>
+            <li>Num 4</li>
+          </ul>
+        </div>
+
+      </div>
+      <div class="copyright">
+        <p>COPYRIGHT 2020 QANTUMTHEMES. CUSTOMIZABLE TEXT.</p>
+      </div>
+    </div>
+  </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
