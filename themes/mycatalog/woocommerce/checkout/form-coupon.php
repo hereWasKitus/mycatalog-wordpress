d16ca883e9a23ffbb8b0e1a43d28da488b539f3c
hereWasKitus <superpants@ex.ua>
2020-10-06 15:08:31 +0300

    Checkout mobile

--- themes/mycatalog/woocommerce/checkout/form-coupon.php
+++ themes/mycatalog/woocommerce/checkout/form-coupon.php
@@ -30,13 +30,15 @@ if ( ! wc_coupons_enabled() ) { // @codingStandardsIgnoreLine.
 
 	<p><?php esc_html_e( 'If you have a coupon code, please apply it below.', 'woocommerce' ); ?></p>
 
-	<p class="form-row form-row-first">
-		<input type="text" name="coupon_code" class="input-text" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" id="coupon_code" value="" />
-	</p>
+	<div class="flex">
+		<p class="form-row form-row-first">
+			<input type="text" name="coupon_code" class="input-text" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" id="coupon_code" value="" />
+		</p>
 
-	<p class="form-row form-row-last">
-		<button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_html_e( 'Apply coupon', 'woocommerce' ); ?></button>
-	</p>
+		<p class="form-row form-row-last">
+			<button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_html_e( 'Apply coupon', 'woocommerce' ); ?></button>
+		</p>
+	</div>
 
 	<div class="clear"></div>
 </form>
