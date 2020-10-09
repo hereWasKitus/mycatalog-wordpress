a39135ff1473efe3016c067ad06642d048187134
hereWasKitus <superpants@ex.ua>
2020-10-09 14:42:37 +0300

    Add rtl styles to slider

--- themes/mycatalog/src/js/libs/slider.js
+++ themes/mycatalog/src/js/libs/slider.js
@@ -1,11 +1,14 @@
 (function () {
 if ( document.querySelector('.slick-last-products') && window.innerWidth <= 1024 ) {
 
+  var is_rtl = wp_data.is_rtl == 1;
+
   $('.slick-last-products').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
-    dots: true
+    dots: true,
+    rtl: is_rtl
   })
 
 }
